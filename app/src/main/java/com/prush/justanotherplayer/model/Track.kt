7c68f84d5bd146c0a7e20e9535064bc9cb4b0192
Puru Pawar <iamporus@gmail.com>
2019-08-30 23:41:02 +0530

    Following SRP for Track.kt. Refactored TrackRepository.kt to move Track related logic to Track class.

--- app/src/main/java/com/prush/justanotherplayer/model/Track.kt
+++ app/src/main/java/com/prush/justanotherplayer/model/Track.kt
@@ -1,5 +1,104 @@
 package com.prush.justanotherplayer.model
 
+import android.content.ContentUris
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.provider.MediaStore
+import java.io.File
+import java.io.FileNotFoundException
 import java.io.Serializable
 
-data class Track(val id: Long, val title: String) : Serializable
+//static fields
+private val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
+private const val selection =
+    MediaStore.Audio.Media.IS_MUSIC + "=1 OR " + MediaStore.Audio.Media.IS_PODCAST + "=1"
+
+private val projection = arrayOf(
+    MediaStore.Audio.Media._ID,
+    MediaStore.Audio.Media.DATA,
+    MediaStore.Audio.Media.TITLE,
+    MediaStore.Audio.Media.ARTIST_ID,
+    MediaStore.Audio.Media.ARTIST,
+    MediaStore.Audio.Media.ALBUM_ID,
+    MediaStore.Audio.Media.ALBUM,
+    MediaStore.Audio.Media.DURATION,
+    MediaStore.Audio.Media.YEAR,
+    MediaStore.Audio.Media.TRACK,
+    MediaStore.Audio.Media.DATE_ADDED,
+    MediaStore.Audio.Media.IS_PODCAST
+)
+
+//static method
+fun getQuery(context: Context): Cursor? {
+
+    return context.contentResolver.query(
+        uri,
+        projection,
+        selection,
+        null,
+        MediaStore.Audio.Media.TRACK
+    )
+}
+
+open class Track(cursor: Cursor) : Serializable {
+
+    var id: Long = 0
+    var artistId: Long = 0
+    var albumId: Long = 0
+    var duration: Long = 0
+    var title: String
+    var artistName: String
+    var albumName: String
+
+    init {
+        id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+        title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+        artistId = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST_ID))
+        artistName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+        albumId = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID))
+        albumName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM))
+        duration = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION))
+    }
+
+    fun getPlaybackUri(): Uri {
+
+        return ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
+    }
+
+    fun getAlbumArtUri(context: Context): Uri {
+
+        val contentUri =
+            ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, albumId)
+
+        val cursor = context.contentResolver
+            .query(
+                contentUri,
+                arrayOf(MediaStore.Audio.Albums.ALBUM_ART),
+                null,
+                null,
+                null
+            )
+
+        if (cursor != null) {
+            try {
+                if (cursor.moveToFirst()) {
+                    val file =
+                        File(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART)))
+                    if (file.exists()) {
+                        try {
+                            return Uri.fromFile(file)
+                        } catch (ignored: FileNotFoundException) {
+                        }
+                    }
+                }
+            } catch (ignored: NullPointerException) {
+            } finally {
+                cursor.close()
+            }
+        }
+
+        return Uri.EMPTY
+    }
+}
--- app/src/main/java/com/prush/justanotherplayer/repositories/TrackRepository.kt
+++ app/src/main/java/com/prush/justanotherplayer/repositories/TrackRepository.kt
@@ -2,9 +2,9 @@ package com.prush.justanotherplayer.repositories
 
 import android.content.Context
 import android.database.Cursor
-import android.provider.MediaStore
 import android.util.Log
 import com.prush.justanotherplayer.model.Track
+import com.prush.justanotherplayer.model.getQuery
 
 class TrackRepository(private val context: Context) : ITrackRepository {
 
@@ -13,27 +13,7 @@ class TrackRepository(private val context: Context) : ITrackRepository {
 
         val trackList: MutableList<Track> = mutableListOf()
 
-        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-
-        val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
-
-        val projection = arrayOf(
-            MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.ARTIST,
-            MediaStore.Audio.Media.TITLE,
-            MediaStore.Audio.Media.DATA,
-            MediaStore.Audio.Media.DISPLAY_NAME,
-            MediaStore.Audio.Media.DURATION,
-            MediaStore.Audio.Media.ALBUM_ID
-        )
-
-        val cursor: Cursor? = context.contentResolver.query(
-            uri,
-            projection,
-            selection,
-            null,
-            null
-        )
+        val cursor: Cursor? = getQuery(context)
 
         when {
             cursor == null -> {
@@ -46,14 +26,8 @@ class TrackRepository(private val context: Context) : ITrackRepository {
             }
             else -> {
 
-                val titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE)
-                val idColumn = cursor.getColumnIndex(MediaStore.Audio.Media._ID)
-
                 do {
-                    val songId = cursor.getLong(idColumn)
-                    val songTitle = cursor.getString(titleColumn)
-
-                    trackList.add(Track(songId, songTitle))
+                    trackList.add(Track(cursor))
 
                 } while (cursor.moveToNext())
 
