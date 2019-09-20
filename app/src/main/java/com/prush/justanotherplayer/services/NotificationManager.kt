5c46ca51907aee32ca70f331ff1eb7da243e9fca
Puru Pawar <iamporus@gmail.com>
2019-09-20 10:17:38 +0530

    Refactored notification handling in respective class.

---
+++ app/src/main/java/com/prush/justanotherplayer/services/NotificationManager.kt
@@ -0,0 +1,76 @@
+package com.prush.justanotherplayer.services
+
+import android.app.Notification
+import android.content.Context
+import android.support.v4.media.session.MediaSessionCompat
+
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.prush.justanotherplayer.R
+import com.prush.justanotherplayer.model.Track
+
+class NotificationManager(
+    context: Context,
+    private val mediaSession: MediaSessionCompat,
+    private val player: SimpleExoPlayer,
+    private val listener: OnNotificationPostedListener
+) {
+
+    private var playerNotificationManager: PlayerNotificationManager
+
+    init {
+        playerNotificationManager =
+            PlayerNotificationManager.createWithNotificationChannel(
+                context,
+                PLAYBACK_CHANNEL_ID,
+                R.string.app_name,
+                R.string.app_name,
+                1,
+                null
+            )
+
+    }
+
+    interface OnNotificationPostedListener {
+        fun onNotificationPosted(notificationId: Int, notification: Notification?)
+
+        fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean)
+    }
+
+    fun setupPlayerNotification(context: Context, tracksList: List<Track>) {
+
+        playerNotificationManager =
+            PlayerNotificationManager.createWithNotificationChannel(
+                context,
+                PLAYBACK_CHANNEL_ID,
+                R.string.app_name,
+                R.string.app_name,
+                1,
+                MediaDescriptionAdapter(context, tracksList),
+
+                object : PlayerNotificationManager.NotificationListener {
+                    override fun onNotificationPosted(
+                        notificationId: Int,
+                        notification: Notification?,
+                        ongoing: Boolean
+                    ) {
+                        listener.onNotificationPosted(notificationId, notification)
+                    }
+
+                    override fun onNotificationCancelled(
+                        notificationId: Int, dismissedByUser: Boolean
+                    ) {
+                        listener.onNotificationCancelled(notificationId, dismissedByUser)
+                    }
+                })
+
+        playerNotificationManager.setPlayer(player)
+
+        mediaSession.isActive = true
+        playerNotificationManager.setMediaSessionToken(mediaSession.sessionToken)
+    }
+
+    fun cleanup() {
+        playerNotificationManager.setPlayer(null)
+    }
+}
