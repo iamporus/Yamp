92fc6cced4049ce07fa52f11b506f7dc749fcc3a
Puru Pawar <iamporus@gmail.com>
2019-10-02 14:58:45 +0530

    Fixed showing walkthrough for first open instance only.

--- app/src/main/java/com/prush/justanotherplayer/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/prush/justanotherplayer/ui/splash/SplashActivity.kt
@@ -1,21 +1,38 @@
 package com.prush.justanotherplayer.ui.splash
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.prush.justanotherplayer.R
+import com.prush.justanotherplayer.ui.main.MainActivity
 import com.prush.justanotherplayer.ui.walkthrough.WalkThroughActivity
+
+private const val KEY_FIRST_INSTANCE = "firstInstance"
+private const val SPLASH_DURATION = 800L
 
 class SplashActivity : AppCompatActivity(), SplashActivityView {
 
 
     override fun gotoMainActivity() {
         Handler().postDelayed({
-            val intent = Intent(this, WalkThroughActivity::class.java)
-            startActivity(intent)
+
+            val sharedPreferences =
+                getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
+
+            if (sharedPreferences.getBoolean(KEY_FIRST_INSTANCE, true)) {
+                sharedPreferences.edit().putBoolean(KEY_FIRST_INSTANCE, false).apply()
+
+                val intent = Intent(this, WalkThroughActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
             finish()
-        }, 800)
+
+        }, SPLASH_DURATION)
 
     }
 
