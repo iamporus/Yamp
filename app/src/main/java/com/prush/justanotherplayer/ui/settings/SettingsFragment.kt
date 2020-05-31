b8577d02727c0237d1ca1c4601ed41048423c5d8
iamporus <iamporus@gmail.com>
2020-05-31 20:58:03 +0530

    added missing support for reading version from build.gradle

--- app/src/main/java/com/prush/justanotherplayer/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/prush/justanotherplayer/ui/settings/SettingsFragment.kt
@@ -1,12 +1,15 @@
 package com.prush.justanotherplayer.ui.settings
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.prush.justanotherplayer.BuildConfig
 import com.prush.justanotherplayer.R
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -21,6 +24,13 @@ class SettingsFragment : PreferenceFragmentCompat() {
             val intent = Intent(activity, OssLicensesMenuActivity::class.java)
             startActivity(intent)
             true
+        }
+
+        val versionPreference = findPreference<Preference>("version")
+        try {
+            versionPreference?.summary = BuildConfig.VERSION_NAME
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
     }
 
